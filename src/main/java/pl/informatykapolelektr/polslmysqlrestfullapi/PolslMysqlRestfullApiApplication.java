f739b8f62be9c1780bb50a9c3ca666bdb7133cff
Miłosz Gilga <gilgamilosz451@gmail.com>
2021-12-20 13:54:27 +0100

    initial commit

---
+++ src/main/java/pl/informatykapolelektr/polslmysqlrestfullapi/PolslMysqlRestfullApiApplication.java
@@ -0,0 +1,13 @@
+package pl.informatykapolelektr.polslmysqlrestfullapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolslMysqlRestfullApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolslMysqlRestfullApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/informatykapolelektr/polslmysqlrestfullapi/PolslMysqlRestfullApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.informatykapolelektr.polslmysqlrestfullapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolslMysqlRestfullApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
