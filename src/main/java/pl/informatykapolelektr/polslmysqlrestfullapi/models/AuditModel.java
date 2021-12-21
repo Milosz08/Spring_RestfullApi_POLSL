f697faf8040b1da6e19ce82e649e79d554d19fee
Miłosz Gilga <gilgamilosz451@gmail.com>
2021-12-21 23:57:21 +0100

    created AuditModel abstract class (implements in all entities)

---
+++ src/main/java/pl/informatykapolelektr/polslmysqlrestfullapi/models/AuditModel.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2021, by Miłosz Gilga <https://miloszgilga.pl>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     <http://www.apache.org/license/LICENSE-2.0>
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the license.
+ */
+
+package pl.informatykapolelektr.polslmysqlrestfullapi.models;
+
+import com.fasterxml.jackson.annotation.*;
+import lombok.*;
+import org.springframework.data.annotation.*;
+import org.springframework.data.jpa.domain.support.*;
+
+import javax.persistence.*;
+import java.io.*;
+import java.util.*;
+
+@Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class AuditModel implements Serializable {
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreatedDate
+    @JsonIgnore
+    private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at", nullable = false)
+    @LastModifiedDate
+    @JsonIgnore
+    private Date updatedAt;
+
+}
