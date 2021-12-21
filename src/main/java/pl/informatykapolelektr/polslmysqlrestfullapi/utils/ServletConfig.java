c32f84fc59adda9c4d57e15269e089d66ec68c18
Miłosz Gilga <gilgamilosz451@gmail.com>
2021-12-22 00:00:37 +0100

    create ServletTime java helper class

---
+++ src/main/java/pl/informatykapolelektr/polslmysqlrestfullapi/utils/ServletConfig.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2021-2021, by Miłosz Gilga <https://miloszgilga.pl>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     <http://www.apache.org/license/LICENSE-2.0>
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the license.
+ */
+
+package pl.informatykapolelektr.polslmysqlrestfullapi.utils;
+
+public class ServletConfig {
+
+    // default app prefix
+    public static final String DEF_PREFIX = "/api/v2/javabean/";
+
+    // app endpoints
+    public static final String CREDENTIALS = "credentials";
+    public static final String CALENDAR = "calendar";
+}
