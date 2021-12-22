2c089d276f1e65a491411114fb9e011a29bc0353
Miłosz Gilga <gilgamilosz451@gmail.com>
2021-12-22 11:27:41 +0100

    add JPA classes for helper links endpoint

---
+++ src/main/java/pl/informatykapolelektr/polslmysqlrestfullapi/controller/HelperLinkController.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2021, by Miłosz Gilga <https://miloszgilga.pl>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     <http://www.apache.org/license/LICENSE-2.0>
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the license.
+ */
+
+package pl.informatykapolelektr.polslmysqlrestfullapi.controller;
+
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import pl.informatykapolelektr.polslmysqlrestfullapi.models.*;
+import pl.informatykapolelektr.polslmysqlrestfullapi.service.*;
+
+import javax.validation.*;
+import java.util.*;
+
+import static pl.informatykapolelektr.polslmysqlrestfullapi.utils.ServletConfig.*;
+
+@RestController
+@RequestMapping(DEF_PREFIX + HELPER_LINKS)
+public class HelperLinkController {
+
+    private final HelperLinkService helperLinkService;
+
+    public HelperLinkController(HelperLinkService helperLinkService) {
+        this.helperLinkService = helperLinkService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<HelperLink>> getAllHelperLinks() {
+        return new ResponseEntity<>(helperLinkService.getAllHelperLinks(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<HelperLink> getSingleHelperLink(@PathVariable String id) {
+        return new ResponseEntity<>(helperLinkService.getSingleHelperLink(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<HelperLink> addHelperLink(@Valid @RequestBody HelperLink helperLink) {
+        return new ResponseEntity<>(helperLinkService.addHelperLink(helperLink), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<HelperLink> editHelperLink(@PathVariable String id, @Valid @RequestBody HelperLink helperLink) {
+        return new ResponseEntity<>(helperLinkService.editHelperLink(id, helperLink), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteSingleHelperLink(@PathVariable String id) {
+        helperLinkService.deleteSingleHelperLink(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteAllHelperLinks() {
+        helperLinkService.deleteAllHelperLink();
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
---
+++ src/main/java/pl/informatykapolelektr/polslmysqlrestfullapi/service/implementation/HelperLinkServiceImplementation.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2021, by Miłosz Gilga <https://miloszgilga.pl>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     <http://www.apache.org/license/LICENSE-2.0>
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the license.
+ */
+
+package pl.informatykapolelektr.polslmysqlrestfullapi.service.implementation;
+
+import java.util.*;
+import org.springframework.stereotype.*;
+
+import pl.informatykapolelektr.polslmysqlrestfullapi.models.*;
+import pl.informatykapolelektr.polslmysqlrestfullapi.repository.*;
+import pl.informatykapolelektr.polslmysqlrestfullapi.service.*;
+import pl.informatykapolelektr.polslmysqlrestfullapi.utils.*;
+
+@Service
+public class HelperLinkServiceImplementation implements HelperLinkService {
+
+    private final HelperLinkRepository helperLinkRepository;
+    private final IconRepository iconRepository;
+
+    public HelperLinkServiceImplementation(HelperLinkRepository helperLinkRepository, IconRepository iconRepository) {
+        this.helperLinkRepository = helperLinkRepository;
+        this.iconRepository = iconRepository;
+    }
+
+    private HelperLink addOrUpdate(HelperLink helperLink) {
+        helperLink.getHelperIcon().set_id(new RandomHexGenerator().generateSequence());
+        helperLinkRepository.save(helperLink);
+        iconRepository.save(helperLink.getHelperIcon());
+        return helperLink;
+    }
+
+    @Override
+    public List<HelperLink> getAllHelperLinks() {
+        return helperLinkRepository.findAll();
+    }
+
+    @Override
+    public HelperLink getSingleHelperLink(String id) {
+        Optional<HelperLink> helperLinkFind = helperLinkRepository.findById(id);
+        if(helperLinkFind.isPresent()) {
+            return helperLinkFind.get();
+        }
+        throw new RuntimeException("Link not found for the ID: " + id);
+    }
+
+    @Override
+    public HelperLink addHelperLink(HelperLink helperLink) {
+        return addOrUpdate(helperLink);
+    }
+
+    @Override
+    public HelperLink editHelperLink(String id, HelperLink helperLink) {
+        Optional<HelperLink> helperLinkFind = helperLinkRepository.findById(id);
+        helperLink.set_id(id);
+        if (helperLinkFind.isPresent()) {
+            return addOrUpdate(helperLink);
+        }
+        throw new RuntimeException("Link not found for the ID: " + id);
+    }
+
+    @Override
+    public void deleteSingleHelperLink(String id) {
+        helperLinkRepository.deleteById(id);
+    }
+
+    @Override
+    public void deleteAllHelperLink() {
+        helperLinkRepository.deleteAll();
+    }
+
+}
