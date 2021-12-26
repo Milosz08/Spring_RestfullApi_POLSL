f434cc4dfa604428d10967d147583b49b4c4e353
Miłosz Gilga <gilgamilosz451@gmail.com>
2021-12-26 19:04:49 +0100

    create classes for custom exceptions' handler in service-layer

---
+++ src/main/java/pl/informatykapolelektr/polslmysqlrestfullapi/exceptions/ApiExceptionHandler.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2021, by Miłosz Gilga <https://miloszgilga.pl>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     <http://www.apache.org/license/LICENSE-2.0>
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the license.
+ */
+
+package pl.informatykapolelektr.polslmysqlrestfullapi.exceptions;
+
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(value = { ApiRequestException.class })
+    public ResponseEntity<Object> handleApiRequestException(ApiRequestException e) {
+        HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+        ApiException apiException = new ApiException(
+                e.getMessage(),
+                badRequest.value(),
+                System.currentTimeMillis()
+        );
+        return new ResponseEntity<>(apiException, badRequest);
+    }
+
+}
--- src/main/java/pl/informatykapolelektr/polslmysqlrestfullapi/exceptions/ApiRequestException.java
+++ src/main/java/pl/informatykapolelektr/polslmysqlrestfullapi/exceptions/ApiRequestException.java
@@ -14,22 +14,10 @@
 
 package pl.informatykapolelektr.polslmysqlrestfullapi.exceptions;
 
-import lombok.*;
-import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
+public class ApiRequestException extends RuntimeException {
 
-@Getter
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException extends RuntimeException {
-
-    private final String resourceName;
-    private final String fieldName;
-    private final String fieldValue;
-
-    public ResourceNotFoundException(String resourceName, String fieldName, String fieldValue) {
-        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
-        this.resourceName = resourceName;
-        this.fieldName = fieldName;
-        this.fieldValue = fieldValue;
+    public ApiRequestException(String message) {
+        super(message);
     }
+
 }
