5f9f450b045d1377af5725d88ff2e2855801228c
Miłosz Gilga <gilgamilosz451@gmail.com>
2021-12-26 15:50:20 +0100

    rewrite many-to-many single-directional inheritance

--- src/main/java/pl/informatykapolelektr/polslmysqlrestfullapi/service/implementation/SubjectsServiceImplementation.java
+++ src/main/java/pl/informatykapolelektr/polslmysqlrestfullapi/service/implementation/SubjectsServiceImplementation.java
@@ -21,33 +21,31 @@ import pl.informatykapolelektr.polslmysqlrestfullapi.service.*;
 import pl.informatykapolelektr.polslmysqlrestfullapi.utils.*;
 
 import java.util.*;
-import java.util.stream.*;
 
 @Service
 public class SubjectsServiceImplementation implements SubjectService {
 
     private final SubjectRepository subjectRepository;
     private final SemesterRepository semesterRepository;
-    private final IconRepository iconRepository;
     private final DepartmentRepository departmentRepository;
     private final ClassesItemRepository classesItemRepository;
 
-    public SubjectsServiceImplementation(SubjectRepository subjectRepository, SemesterRepository semesterRepository,
-        IconRepository iconRepository, DepartmentRepository departmentRepository,
+    public SubjectsServiceImplementation(SubjectRepository subjectRepository,
+        SemesterRepository semesterRepository, DepartmentRepository departmentRepository,
         ClassesItemRepository classesItemRepository
     ) {
         this.subjectRepository = subjectRepository;
         this.semesterRepository = semesterRepository;
-        this.iconRepository = iconRepository;
         this.departmentRepository = departmentRepository;
         this.classesItemRepository = classesItemRepository;
     }
 
     private Subject addOrUpdate(Subject subject) {
         subject.getIcon().set_id(new RandomHexGenerator().generateSequence());
-        //for(Semester item : subject.getSemesters()) {
-        //    item.set_id(new RandomHexGenerator().generateSequence());
-        //}
+        ManyToManyManageEntities.addOrUpdateSemesters(subject, semesterRepository);
+        ManyToManyManageEntities.addOrUpdateDepartments(subject, departmentRepository);
+        ManyToManyManageEntities.addOrUpdateClassesItems(subject, classesItemRepository);
+        subjectRepository.save(subject);
         return subject;
     }
 
@@ -59,36 +57,21 @@ public class SubjectsServiceImplementation implements SubjectService {
     @Override
     public Subject getSingleSubject(String id) {
         Optional<Subject> subjectFind = subjectRepository.findById(id);
-        if(subjectFind.isPresent()) {
-            return addOrUpdate(subjectFind.get());
+        if (subjectFind.isPresent()) {
+            return subjectFind.get();
         }
         throw new RuntimeException("Subject not found for the id: " + id);
     }
 
     @Override
     public Subject addSubject(Subject subject) {
-        subject.getIcon().set_id(new RandomHexGenerator().generateSequence());
-        //for(Semester item : subject.getSemesters()) {
-        //    item.set_id(new RandomHexGenerator().generateSequence());
-        //}
-        //for(Department item : subject.getDepartments()) {
-        //   item.set_id(new RandomHexGenerator().generateSequence());
-        //}
-        //for(ClassesItem item : subject.getClassesPlatform()) {
-        //    item.set_id(new RandomHexGenerator().generateSequence());
-       //     System.out.println(item.get_id());
-       // }
-        semesterRepository.saveAll(subject.getSemesters());
-        departmentRepository.saveAll(subject.getDepartments());
-        classesItemRepository.saveAll(subject.getClassesPlatform());
-        subjectRepository.save(subject);
-        return subject;
+        return addOrUpdate(subject);
     }
 
     @Override
     public Subject editSubject(String id, Subject subject) {
         Optional<Subject> subjectFind = subjectRepository.findById(id);
-        if(subjectFind.isPresent()) {
+        if (subjectFind.isPresent()) {
             subject.set_id(id);
             return addOrUpdate(subject);
         }
@@ -97,11 +80,15 @@ public class SubjectsServiceImplementation implements SubjectService {
 
     @Override
     public void deleteSingleSubject(String id) {
+
         subjectRepository.deleteById(id);
     }
 
     @Override
     public void deleteAllSubject() {
+        semesterRepository.deleteAll();
+        departmentRepository.deleteAll();
+        classesItemRepository.deleteAll();
         subjectRepository.deleteAll();
     }
 
