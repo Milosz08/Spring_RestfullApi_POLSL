71dce687be55db5351748fd4d2a83077b18f898e
Miłosz Gilga <gilgamilosz451@gmail.com>
2021-12-27 20:59:26 +0100

    rewrite regex pattern and remove min signs

--- src/main/java/pl/informatykapolelektr/polslmysqlrestfullapi/models/CalendarItems.java
+++ src/main/java/pl/informatykapolelektr/polslmysqlrestfullapi/models/CalendarItems.java
@@ -42,15 +42,15 @@ public class CalendarItems extends AuditModel {
     @NotEmpty(message = "Brak/puste pole odpowiadające za rozpoczęcie aktywności wydarzenia kalendarza")
     @Column(name = "start_time")
     @Pattern(
-        regexp = "/^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/",
+        regexp = "^([0-1]?[0-9]|[2][0-3]):([0-5][0-9])(:[0-5][0-9])?$",
         message = "Pole czasu rozpoczęcia aktywności nie jest w formacie HH:mm"
     )
     private String start;
 
     @NotNull(message = "Brak/puste pole odpowiadające za wiadomość o aktywności wydarzenia kalendarza")
     @Size(
-        min = 10, max = 80,
-        message = "Pole wiadomości o aktywności wydarzenia nie mieści się w przedziale od 10 do 80 znaków"
+        max = 80,
+        message = "Pole wiadomości o aktywności wydarzenia nie mieści się w przedziale do 80 znaków"
     )
     @Column(name = "calendar_item_message")
     private String message;
